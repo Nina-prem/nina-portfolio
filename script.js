ccd07038ce0d609b11669fe976e4095c7a6d0983
unknown <nainapremraj@gmail.comgit config --global user.nameNina-prem>
2026-09-14 19:09:30 +0530

    Update dashboard files

--- script.js
+++ script.js
@@ -109,8 +109,11 @@ revealElements.forEach(element => {
    PROJECT FILTER
 ===================================================== */
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> 17b6af4 (Update dashboard files)
 const filters = document.querySelectorAll(".filter");
 const projectCards = document.querySelectorAll(".project-card");
 
